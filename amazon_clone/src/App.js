5217b9a2f43ddfee1514ac0582a73a24cf059a9a
Yeabsira <yeabsiratamirat0626@gmail.com>
2026-02-23 13:50:19 -0800

    Adding carousel effect

--- amazon_clone/src/App.js
+++ amazon_clone/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Carousel from './Components/Carousel/Carousel';
 import Header from './Components/Header/Header';
 
 function App() {
   return (
     <>
       <Header />
+      <Carousel />
     </>
   );
 }

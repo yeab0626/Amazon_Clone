9213c6f92ff21223e0cc0f679fdf82aaa7d86dfd
Yeabsira <yeabsiratamirat0626@gmail.com>
2026-03-12 11:20:48 -0700

    cart page part two

--- amazon_clone/src/Pages/Cart/Cart.jsx
+++ amazon_clone/src/Pages/Cart/Cart.jsx
@@ -9,9 +9,10 @@ import classes from './Cart.module.css'
 function Cart() {
    const [{basket, user}, dispatch] = useContext(DataContext)
    const total = basket.reduce((amount, item )=>{
-      return item.price + amount ;
+      return item.price * item.amount + amount ;
        },0)
-
+ console.log(basket)
+ 
 
   return (
     <LayOut>
--- amazon_clone/src/Utility/reducer.js
+++ amazon_clone/src/Utility/reducer.js
@@ -11,11 +11,26 @@ export const initialState = {
 export const reducer = (state, action)=>{
    switch (action.type) {
     case Type.ADD_TO_BASKET:
-        return {
-            ...state,
-            basket:[...state.basket, action.item]
-        }
+        // return {
+        //     ...state,
+        //     basket:[...state.basket, action.item]
+        // }
+        const existingItem = state.basket.find((item)=>item.id === action.item.id)
+         if(!existingItem){
+            return {
+                ...state,
+                basket: [...state.basket, {...action.item, amount:1}]
+            }
+         }else {
+            const updatedBasket = state.basket.map((item)=>{
+            return    item.id  ===action.item.id? {...item,amount:item.amount + 1} : item
+            })
 
+            return{
+                ...state,
+                basket :updatedBasket
+            }
+         }
    
     default:
         return state;

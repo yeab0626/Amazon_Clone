c6ce332a80e985acccff4bd21a9fa527440f20f7
Yeabsira <yeabsiratamirat0626@gmail.com>
2026-03-18 12:39:18 -0700

    refactoring backend payment API without functions

---
+++ amzone-api/index.js
@@ -0,0 +1,44 @@
+const express = require ("express");
+const cors = require ("cors");
+const dotenv = require("dotenv");
+dotenv.config()
+const stripe = require("stripe")(process.env.STRIPE_KEY);
+
+
+const app = express ()
+app.use(cors({origin:true}))
+
+app.use(express.json())
+
+app.get("/", (req,res)=> {
+    res.status(200).json({
+        Message: "Success !",
+    });
+});
+
+app.post("/payment/create", async(req, res) => {
+    const total = req.query.total;
+
+    if (total > 0){
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount: total,
+            currency: "usd"
+        })
+        console.log(paymentIntent);
+        res.status(201).json({clientSecret: paymentIntent.client_secret,
+        });
+    }else{
+         res.status(403).json({
+            message: "total must be greater than 0",
+         })
+    }
+});
+
+const server =app.listen(5000,()=>{
+    console.log("Amazone server running on PORT: 5000, http://localhost:5000 ")
+});
+
+
+server.on("error", (err) => {
+    console.error("Server error:", err);
+});
